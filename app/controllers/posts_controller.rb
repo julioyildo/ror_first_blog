fae70fd3479aaf2fc00fa54a5766290e4b0a62d6
votrepseudojulioyildogit config --global user.email votre@emailserhat.yildirim@hetic.net <serhat.yildirim@hetic.net>
2018-02-17 23:05:04 +0100

    index html > enable Show, Edit and Delete action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
 
-   
+        before_action :authenticate_admin!, except: [:index, :show]
         before_action :find_post, only: [:edit, :update, :show, :delete]
       
         # Index action to render all posts
